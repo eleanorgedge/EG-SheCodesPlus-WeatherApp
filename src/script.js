99caf33921b5b01f069c6b22cd2dd2d59b7b16d8
eleanorgedge <gedgeellie@gmail.com>
2021-02-26 18:57:48 +0000

    Added 6 day forecast

--- src/script.js
+++ src/script.js
@@ -147,8 +147,9 @@ button.addEventListener("click", getCurrentPosition);
 //FORECAST
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+//FORECAST DAY 1  
   forecast = response.data.daily[1];
-  //console.log(response.data);
+  console.log(response.data);
   forecastElement.innerHTML = `
   <div class="row 1">
     <div class="col-6 forecast-day">
@@ -159,7 +160,92 @@ function displayForecast(response) {
     </div>
     <div class="col-2 forecast-icon">
       <img src="https://openweathermap.org/img/wn/${
-        response.data.daily[1].weather[0].icon
+        forecast.weather[0].icon
+      }@2x.png" class="w-100" />
+    </div>
+  </div>
+`;
+//FORECAST DAY 2
+forecast = response.data.daily[2];
+forecastElement.innerHTML += `
+  <div class="row 1">
+    <div class="col-6 forecast-day">
+      ${formatDay(forecast.dt*1000)}
+    </div>
+    <div class="col-4 forecast-temperature">
+      ${Math.round(forecast.temp.day)}° C
+    </div>
+    <div class="col-2 forecast-icon">
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png" class="w-100" />
+    </div>
+  </div>
+`;
+//FORECAST DAY 3
+forecast = response.data.daily[3];
+forecastElement.innerHTML += `
+  <div class="row 1">
+    <div class="col-6 forecast-day">
+      ${formatDay(forecast.dt*1000)}
+    </div>
+    <div class="col-4 forecast-temperature">
+      ${Math.round(forecast.temp.day)}° C
+    </div>
+    <div class="col-2 forecast-icon">
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png" class="w-100" />
+    </div>
+  </div>
+`;
+//FORECAST DAY 4
+forecast = response.data.daily[4];
+forecastElement.innerHTML += `
+  <div class="row 1">
+    <div class="col-6 forecast-day">
+      ${formatDay(forecast.dt*1000)}
+    </div>
+    <div class="col-4 forecast-temperature">
+      ${Math.round(forecast.temp.day)}° C
+    </div>
+    <div class="col-2 forecast-icon">
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png" class="w-100" />
+    </div>
+  </div>
+`;
+//FORECAST DAY 5
+forecast = response.data.daily[5];
+forecastElement.innerHTML += `
+  <div class="row 1">
+    <div class="col-6 forecast-day">
+      ${formatDay(forecast.dt*1000)}
+    </div>
+    <div class="col-4 forecast-temperature">
+      ${Math.round(forecast.temp.day)}° C
+    </div>
+    <div class="col-2 forecast-icon">
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png" class="w-100" />
+    </div>
+  </div>
+`;
+//FORECAST DAY 6
+forecast = response.data.daily[6];
+forecastElement.innerHTML += `
+  <div class="row 1">
+    <div class="col-6 forecast-day">
+      ${formatDay(forecast.dt*1000)}
+    </div>
+    <div class="col-4 forecast-temperature">
+      ${Math.round(forecast.temp.day)}° C
+    </div>
+    <div class="col-2 forecast-icon">
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
       }@2x.png" class="w-100" />
     </div>
   </div>
